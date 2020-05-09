e531b23d42d80cf380c4aa2cec34b9389fe21b85
lleldigm <lleldigm@gmail.com>
2020-05-09 20:06:05 +0900

    初回コミット。テストも兼ねてフォームのText変更。

--- Item_dbEditer2020/Item_dbEditer2020/Form1.cs
+++ Item_dbEditer2020/Item_dbEditer2020/Form1.cs
@@ -16,5 +16,10 @@ namespace Item_dbEditer2020
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
