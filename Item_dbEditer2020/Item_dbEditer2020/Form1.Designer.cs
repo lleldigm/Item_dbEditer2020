329965ef7b9f33fba25397a0aa4e2eace05c5af9
lleldigm <lleldigm@gmail.com>
2020-05-09 20:42:35 +0900

    panelMainBaseCommon部ラベル入れ完了

--- Item_dbEditer2020/Item_dbEditer2020/Form1.Designer.cs
+++ Item_dbEditer2020/Item_dbEditer2020/Form1.Designer.cs
@@ -30,16 +30,27 @@
         {
             this.panelItemList = new System.Windows.Forms.Panel();
             this.panelMain = new System.Windows.Forms.Panel();
-            this.panelMainBase = new System.Windows.Forms.Panel();
-            this.panelMainCard = new System.Windows.Forms.Panel();
-            this.panelMainArrow = new System.Windows.Forms.Panel();
-            this.panelMainValue = new System.Windows.Forms.Panel();
-            this.panelMainDb2 = new System.Windows.Forms.Panel();
             this.panelMainScript = new System.Windows.Forms.Panel();
-            this.panelMainBaseCommon = new System.Windows.Forms.Panel();
+            this.panelMainDb2 = new System.Windows.Forms.Panel();
+            this.panelMainValue = new System.Windows.Forms.Panel();
+            this.panelMainArrow = new System.Windows.Forms.Panel();
+            this.panelMainCard = new System.Windows.Forms.Panel();
+            this.panelMainBase = new System.Windows.Forms.Panel();
             this.panelMainBaseEquip = new System.Windows.Forms.Panel();
+            this.panelMainBaseCommon = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelId = new System.Windows.Forms.Label();
             this.panelMain.SuspendLayout();
             this.panelMainBase.SuspendLayout();
+            this.panelMainBaseCommon.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelItemList
@@ -64,6 +75,46 @@
             this.panelMain.Size = new System.Drawing.Size(817, 618);
             this.panelMain.TabIndex = 1;
             // 
+            // panelMainScript
+            // 
+            this.panelMainScript.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMainScript.Location = new System.Drawing.Point(200, 0);
+            this.panelMainScript.Name = "panelMainScript";
+            this.panelMainScript.Size = new System.Drawing.Size(617, 67);
+            this.panelMainScript.TabIndex = 5;
+            // 
+            // panelMainDb2
+            // 
+            this.panelMainDb2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelMainDb2.Location = new System.Drawing.Point(200, 67);
+            this.panelMainDb2.Name = "panelMainDb2";
+            this.panelMainDb2.Size = new System.Drawing.Size(617, 218);
+            this.panelMainDb2.TabIndex = 4;
+            // 
+            // panelMainValue
+            // 
+            this.panelMainValue.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelMainValue.Location = new System.Drawing.Point(200, 285);
+            this.panelMainValue.Name = "panelMainValue";
+            this.panelMainValue.Size = new System.Drawing.Size(617, 190);
+            this.panelMainValue.TabIndex = 3;
+            // 
+            // panelMainArrow
+            // 
+            this.panelMainArrow.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelMainArrow.Location = new System.Drawing.Point(200, 475);
+            this.panelMainArrow.Name = "panelMainArrow";
+            this.panelMainArrow.Size = new System.Drawing.Size(617, 73);
+            this.panelMainArrow.TabIndex = 2;
+            // 
+            // panelMainCard
+            // 
+            this.panelMainCard.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelMainCard.Location = new System.Drawing.Point(200, 548);
+            this.panelMainCard.Name = "panelMainCard";
+            this.panelMainCard.Size = new System.Drawing.Size(617, 70);
+            this.panelMainCard.TabIndex = 1;
+            // 
             // panelMainBase
             // 
             this.panelMainBase.Controls.Add(this.panelMainBaseEquip);
@@ -74,54 +125,6 @@
             this.panelMainBase.Size = new System.Drawing.Size(200, 618);
             this.panelMainBase.TabIndex = 0;
             // 
-            // panelMainCard
-            // 
-            this.panelMainCard.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelMainCard.Location = new System.Drawing.Point(200, 548);
-            this.panelMainCard.Name = "panelMainCard";
-            this.panelMainCard.Size = new System.Drawing.Size(617, 70);
-            this.panelMainCard.TabIndex = 1;
-            // 
-            // panelMainArrow
-            // 
-            this.panelMainArrow.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelMainArrow.Location = new System.Drawing.Point(200, 475);
-            this.panelMainArrow.Name = "panelMainArrow";
-            this.panelMainArrow.Size = new System.Drawing.Size(617, 73);
-            this.panelMainArrow.TabIndex = 2;
-            // 
-            // panelMainValue
-            // 
-            this.panelMainValue.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelMainValue.Location = new System.Drawing.Point(200, 285);
-            this.panelMainValue.Name = "panelMainValue";
-            this.panelMainValue.Size = new System.Drawing.Size(617, 190);
-            this.panelMainValue.TabIndex = 3;
-            // 
-            // panelMainDb2
-            // 
-            this.panelMainDb2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelMainDb2.Location = new System.Drawing.Point(200, 67);
-            this.panelMainDb2.Name = "panelMainDb2";
-            this.panelMainDb2.Size = new System.Drawing.Size(617, 218);
-            this.panelMainDb2.TabIndex = 4;
-            // 
-            // panelMainScript
-            // 
-            this.panelMainScript.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelMainScript.Location = new System.Drawing.Point(200, 0);
-            this.panelMainScript.Name = "panelMainScript";
-            this.panelMainScript.Size = new System.Drawing.Size(617, 67);
-            this.panelMainScript.TabIndex = 5;
-            // 
-            // panelMainBaseCommon
-            // 
-            this.panelMainBaseCommon.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelMainBaseCommon.Location = new System.Drawing.Point(0, 0);
-            this.panelMainBaseCommon.Name = "panelMainBaseCommon";
-            this.panelMainBaseCommon.Size = new System.Drawing.Size(200, 231);
-            this.panelMainBaseCommon.TabIndex = 0;
-            // 
             // panelMainBaseEquip
             // 
             this.panelMainBaseEquip.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -129,6 +132,114 @@
             this.panelMainBaseEquip.Name = "panelMainBaseEquip";
             this.panelMainBaseEquip.Size = new System.Drawing.Size(200, 387);
             this.panelMainBaseEquip.TabIndex = 1;
+            // 
+            // panelMainBaseCommon
+            // 
+            this.panelMainBaseCommon.Controls.Add(this.label9);
+            this.panelMainBaseCommon.Controls.Add(this.label8);
+            this.panelMainBaseCommon.Controls.Add(this.label7);
+            this.panelMainBaseCommon.Controls.Add(this.label6);
+            this.panelMainBaseCommon.Controls.Add(this.label5);
+            this.panelMainBaseCommon.Controls.Add(this.label4);
+            this.panelMainBaseCommon.Controls.Add(this.label3);
+            this.panelMainBaseCommon.Controls.Add(this.label2);
+            this.panelMainBaseCommon.Controls.Add(this.label1);
+            this.panelMainBaseCommon.Controls.Add(this.labelId);
+            this.panelMainBaseCommon.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMainBaseCommon.Location = new System.Drawing.Point(0, 0);
+            this.panelMainBaseCommon.Name = "panelMainBaseCommon";
+            this.panelMainBaseCommon.Size = new System.Drawing.Size(200, 231);
+            this.panelMainBaseCommon.TabIndex = 0;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(63, 205);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 12);
+            this.label9.TabIndex = 39;
+            this.label9.Text = "使用/装備可能Lv";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(119, 177);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "性別";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(43, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 37;
+            this.label7.Text = "重量";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(117, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "売値";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "販売価格";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "職業制限";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "種類";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "日本名";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "英語名";
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(44, 14);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(16, 12);
+            this.labelId.TabIndex = 30;
+            this.labelId.Text = "ID";
             // 
             // Form1
             // 
@@ -142,6 +253,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panelMain.ResumeLayout(false);
             this.panelMainBase.ResumeLayout(false);
+            this.panelMainBaseCommon.ResumeLayout(false);
+            this.panelMainBaseCommon.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -158,6 +271,16 @@
         private System.Windows.Forms.Panel panelMainBase;
         private System.Windows.Forms.Panel panelMainBaseEquip;
         private System.Windows.Forms.Panel panelMainBaseCommon;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelId;
     }
 }
 
